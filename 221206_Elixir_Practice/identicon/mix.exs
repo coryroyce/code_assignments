36309c1a5b6bba8cadaa25b3d15695169b258e61
Cory Randolph <coryroyce@gmail.com>
2022-12-19 14:46:58 -0800

    Adding to Identicon

--- 221206_Elixir_Practice/identicon/mix.exs
+++ 221206_Elixir_Practice/identicon/mix.exs
@@ -7,20 +7,22 @@ defmodule Identicon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      exclude: [:crypto]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :crypto]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:egd, github: "erlang/egd", manager: :rebar3}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
